3dc2b69c297dee3e45ac9ca69d3018b6cd2e49d2
wirawanhd <wirawanh47@gmail.com>
2023-10-30 15:35:16 +0700

    save aja

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -6,7 +6,6 @@ const Header = () => {
         <header className="bg-indigo-500 fixed z-50 w-full">
             <div className="flex md:flex-row flex-col justify-between p-4 gap-2">
                 <Link href="/" className="font-bold text-white text-2xl">SMP YAPI AL-HUSAENI</Link>
-                <input placeholder="cari anime..." className=""/>
             </div>
         </header>
     )
